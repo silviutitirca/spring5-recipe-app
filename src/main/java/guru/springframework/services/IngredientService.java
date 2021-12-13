35e471af573d2178f076417f5f81d830d46d98af
silviutitirca <s_titirca@yahoo.com>
2021-12-13 17:05:37 +0100

    Added ingredient view and tests.

---
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -0,0 +1,11 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.IngredientCommand;
+
+/**
+ * @author Silviu_Titirca on 12/13/2021
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
